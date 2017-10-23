8ead2b507b601fd2800f6f37d3d7b7002879c04e
shinomatsu <shi968k5@gmail.com>
2017-10-24 00:17:45 +0800

    Add nav ber

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,39 @@
 class UsersController < ApplicationController
+	before_action :set_user,only: [:show,:edit,:update]
+	
 	def index
 	end
 
 	def show
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		
 	end
 
+	def update
+		# @user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to user_path
+		else
+			render :edit
+		end
+
+	end
+
+
+private
+def set_user
+	@user = User.find(params[:id])
+
+
+	end
+
+	def user_params
+		params.require(:user).permit(:first_name,:last_name,:email)
+
+	end
 
   
 end
