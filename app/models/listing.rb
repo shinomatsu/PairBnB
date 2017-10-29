2cf221c49a767dd5d4adbfdea3692c578a03a24b
shinomatsu <shi968k5@gmail.com>
2017-10-29 15:40:35 +0800

    Add navbar

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,14 @@ class Listing < ApplicationRecord
 	has_many :bookings
 	
 	mount_uploaders :photos, PhotoUploader
+
 end
+
+
+ def self.search(search) 
+    if search 
+      Listing.where(['name LIKE ?', "%#{search}%"])
+    else
+      Listing.all #全て表示。
+    end
+  end
