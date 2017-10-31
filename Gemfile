20dd19fc958623414070e957da2ad01ba2780edb
shinomatsu <shi968k5@gmail.com>
2017-10-31 17:33:37 +0800

    Add gemfile sidekiq

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+#active job
+gem 'sidekiq'
+
 #braintree 
 gem 'braintree'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'abc@gmail.com'}
+  config.action_mailer.default_options = {from: 'pairbnbtesttest@gmail.com'}
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 #setting off
 
