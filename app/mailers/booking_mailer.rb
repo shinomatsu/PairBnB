15cb7b458549db54162a45350258b7dc601b9ea8
shinomatsu <shi968k5@gmail.com>
2017-10-31 17:10:40 +0800

    Generate reservation mailer

---
+++ app/mailers/booking_mailer.rb
@@ -0,0 +1,14 @@
+class BookingMailer < ApplicationMailer
+  
+  default from: 'pairbnbtesttest@gmail.com'
+
+  def notification_email(customer, host, listing_id, booking_id)
+    @host = host
+    @customer = customer
+    @listing = Listing.find(listing_id)
+    #once customer reserved a listing, it will send email to the listing host.
+    mail(to: @host, subject: "You have received a booking from #{@customer}")
+
+  end
+
+end
